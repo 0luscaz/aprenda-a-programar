b754995c7d8ce7e534d167515056756e9812a7ac
Luscaz <lfbrayner@gmail.com>
2022-08-23 22:16:25 -0300

    js1-update

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,12 +1,17 @@
 let nome 
+nome =String
 nome = "Lucas Brayner,"
 let email
+email = String
 email = "lfbrayner@gmail.com,"
 let profissao
+profissao = String
 profissao = "Estudante,"
 let idade
-idade = "18 anos,"
+idade = Number
+idade = "18,"
 let localver
+localver = String
 localver = "Acre."
 
 console.log(nome,email,profissao,idade,localver)
