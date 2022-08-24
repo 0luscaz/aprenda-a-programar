ed9a001898c40ba5856d37e9c72e611d4618018a
Luscaz <lfbrayner@gmail.com>
2022-08-23 22:13:05 -0300

    js1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,12 @@
+let nome 
+nome = "Lucas Brayner,"
+let email
+email = "lfbrayner@gmail.com,"
+let profissao
+profissao = "Estudante,"
+let idade
+idade = "18 anos,"
+let localver
+localver = "Acre."
+
+console.log(nome,email,profissao,idade,localver)
